d72201142cac7b6f292d8f8d476785a240b83d04
Chris Pulman <chris.pulman@yahoo.com>
2025-10-04 17:22:46 +0100

    Add tests for Autofac and Ninject Avalonia mixins
    Introduces unit tests for Autofac and Ninject integration with Avalonia's ReactiveUI mixins, including null argument checks and builder return validation. Updates the test project to reference Autofac and Ninject projects with appropriate aliases.

---
+++ src/ReactiveUI.Avalonia.Tests/AppBuilderExtensionsOverloadsTests.cs
@@ -0,0 +1,33 @@
+using Avalonia;
+using NUnit.Framework;
+
+namespace ReactiveUI.Avalonia.Tests
+{
+    public class AppBuilderExtensionsOverloadsTests
+    {
+        [Test]
+        public void UseReactiveUI_WithBuilderOverload_ThrowsOnNulls()
+        {
+            AppBuilder? builder = null;
+            Assert.Throws<ArgumentNullException>(() => builder!.UseReactiveUI(_ => { }));
+
+            var b = AppBuilder.Configure<Application>();
+            Assert.Throws<ArgumentNullException>(() => b.UseReactiveUI(null!));
+        }
+
+        [Test]
+        public void RegisterReactiveUIViews_ThrowsOnNullBuilder()
+        {
+            AppBuilder? builder = null;
+            Assert.Throws<ArgumentNullException>(() => builder!.RegisterReactiveUIViews());
+        }
+
+        [Test]
+        public void RegisterReactiveUIViews_ReturnsBuilder_NoAssemblies_NoThrow()
+        {
+            var builder = AppBuilder.Configure<Application>();
+            var result = builder.RegisterReactiveUIViews();
+            Assert.That(result, Is.SameAs(builder));
+        }
+    }
+}
---
+++ src/ReactiveUI.Avalonia.Tests/AvaloniaMixinsAutofacTests.cs
@@ -0,0 +1,50 @@
+extern alias autofac;
+
+using Avalonia;
+using NUnit.Framework;
+using ReactiveUI.Avalonia;
+using Splat.Autofac;
+using AvaloniaMixins = autofac::Avalonia.ReactiveUI.Splat.AvaloniaMixins;
+
+namespace ReactiveUI.Avalonia.Tests
+{
+    public class AvaloniaMixinsAutofacTests
+    {
+        [Test]
+        public void UseReactiveUIWithAutofac_Extension_ThrowsOnNullBuilder()
+        {
+            AppBuilder? builder = null;
+            Assert.Throws<ArgumentNullException>(() =>
+                AvaloniaMixins.UseReactiveUIWithAutofac(builder!, _ => { }, null, null));
+        }
+
+        [Test]
+        public void UseReactiveUIWithAutofac_Overload_ThrowsOnNullBuilder()
+        {
+            AppBuilder? builder = null;
+            Assert.Throws<ArgumentNullException>(() =>
+                AvaloniaMixins.UseReactiveUIWithAutofac(builder!, _ => { }, null, null));
+        }
+
+        [Test]
+        public void UseReactiveUIWithAutofac_ReturnsBuilder_NoThrow()
+        {
+            var builder = AppBuilder.Configure<Application>();
+            var result = AvaloniaMixins.UseReactiveUIWithAutofac(builder, _ => { }, null, null);
+            Assert.That(result, Is.SameAs(builder));
+        }
+
+        [Test]
+        public void UseReactiveUIWithAutofac_Overload_ReturnsBuilder_NoThrow()
+        {
+            var builder = AppBuilder.Configure<Application>();
+            var result = AvaloniaMixins.UseReactiveUIWithAutofac(
+                builder,
+                _ => { },
+                r => { Assert.That(r, Is.InstanceOf<AutofacDependencyResolver>()); },
+                rx => { Assert.That(rx, Is.Not.Null); });
+
+            Assert.That(result, Is.SameAs(builder));
+        }
+    }
+}
