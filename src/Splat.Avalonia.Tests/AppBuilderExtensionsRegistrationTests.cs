fd730ea19e4081a8d15db436a2c45deeb231b3cc
Chris Pulman <chris.pulman@yahoo.com>
2025-10-04 11:50:30 +0800

    Fix FindRoutedEvent, Add unit tests for Avalonia (#20)
    Added comprehensive unit tests for AvaloniaCreatesCommandBinding, AutoSuspendHelper, view host navigation, and ReactiveUserControl binding. Improved event search in AvaloniaCreatesCommandBinding to traverse the type hierarchy, ensuring routed events declared on base classes are found. These changes enhance test coverage and reliability for Avalonia integration in ReactiveUI.

---
+++ src/Splat.Avalonia.Tests/AppBuilderExtensionsRegistrationTests.cs
@@ -0,0 +1,156 @@
+using System.Reflection;
+using Avalonia;
+using Avalonia.Controls;
+using NUnit.Framework;
+using ReactiveUI;
+using Splat;
+
+namespace ReactiveUI.Avalonia.Tests
+{
+    public class AppBuilderExtensionsRegistrationTests
+    {
+        private interface ITestVm
+        {
+        }
+
+        [SetUp]
+        public void ResetLocator()
+        {
+            Assert.That(AppLocator.CurrentMutable, Is.Not.Null);
+
+            _ = new TestVm();
+            _ = new TestView();
+            _ = new ContractedTestView();
+
+            var serviceType = typeof(IViewFor<>).MakeGenericType(typeof(TestVm));
+            AppLocator.CurrentMutable.UnregisterCurrent(serviceType, null);
+            AppLocator.CurrentMutable.UnregisterCurrent(serviceType, "C1");
+        }
+
+        [Test]
+        public void RegisterViewsInternal_Registers_View_For_ViewModel_Using_Activator()
+        {
+            var resolver = AppLocator.CurrentMutable!;
+            var method = typeof(AppBuilderExtensions)
+                .GetMethod("RegisterViewsInternal", BindingFlags.NonPublic | BindingFlags.Static);
+            Assert.That(method, Is.Not.Null);
+
+            method!.Invoke(null, new object[] { resolver, new[] { typeof(AppBuilderExtensionsRegistrationTests).Assembly } });
+
+            var serviceType = typeof(IViewFor<>).MakeGenericType(typeof(TestVm));
+            var resolved = AppLocator.Current.GetService(serviceType);
+
+            Assert.That(resolved, Is.Not.Null);
+            Assert.That(serviceType.IsInstanceOfType(resolved), Is.True);
+        }
+
+        [Test]
+        public void RegisterViewsInternal_Honors_ViewContractAttribute()
+        {
+            var resolver = AppLocator.CurrentMutable!;
+            var method = typeof(AppBuilderExtensions)
+                .GetMethod("RegisterViewsInternal", BindingFlags.NonPublic | BindingFlags.Static);
+
+            method!.Invoke(null, new object[] { resolver, new[] { typeof(AppBuilderExtensionsRegistrationTests).Assembly } });
+
+            var serviceType = typeof(IViewFor<>).MakeGenericType(typeof(TestVm));
+            var resolvedDefault = AppLocator.Current.GetService(serviceType);
+            var resolvedC1 = AppLocator.Current.GetService(serviceType, "C1");
+
+            Assert.That(resolvedDefault, Is.Not.Null);
+            Assert.That(serviceType.IsInstanceOfType(resolvedDefault), Is.True);
+            Assert.That(resolvedC1, Is.InstanceOf<ContractedTestView>());
+        }
+
+        [Test]
+        public void RegisterViewsInternal_Prefers_DI_Resolution_Over_Activator()
+        {
+            var resolver = AppLocator.CurrentMutable!;
+
+            var diInstance = new DiBackedView();
+            resolver.RegisterConstant(diInstance, typeof(DiBackedView));
+
+            var method = typeof(AppBuilderExtensions)
+                .GetMethod("RegisterViewsInternal", BindingFlags.NonPublic | BindingFlags.Static);
+
+            method!.Invoke(null, new object[] { resolver, new[] { typeof(AppBuilderExtensionsRegistrationTests).Assembly } });
+
+            var serviceType = typeof(IViewFor<>).MakeGenericType(typeof(TestVm));
+            var resolved = AppLocator.Current.GetService(serviceType);
+
+            Assert.That(resolved, Is.SameAs(diInstance));
+        }
+
+        [Test]
+        public void RegisterReactiveUIViews_Throws_On_Null_Builder()
+        {
+            AppBuilder? builder = null;
+            Assert.Throws<ArgumentNullException>(() => builder!.RegisterReactiveUIViews());
+        }
+
+        [Test]
+        public void RegisterReactiveUIViewsFromAssemblyOf_Returns_Same_Builder()
+        {
+            var builder = AppBuilder.Configure<Application>();
+            var result = builder.RegisterReactiveUIViewsFromAssemblyOf<AppBuilderExtensionsRegistrationTests>();
+            Assert.That(result, Is.SameAs(builder));
+        }
+
+        [Test]
+        public void RegisterReactiveUIViewsFromEntryAssembly_Does_Not_Throw()
+        {
+            var builder = AppBuilder.Configure<Application>();
+            var result = builder.RegisterReactiveUIViewsFromEntryAssembly();
+            Assert.That(result, Is.SameAs(builder));
+        }
+
+        private sealed class TestVm : ReactiveObject, ITestVm
+        {
+        }
+
+        [AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
+        private sealed class ViewContractAttribute : Attribute
+        {
+            public ViewContractAttribute(string contract)
+            {
+                Contract = contract;
+            }
+
+            public string Contract { get; }
+        }
+
+        private sealed class TestView : UserControl, IViewFor<TestVm>
+        {
+            public TestVm? ViewModel { get; set; }
+
+            object? IViewFor.ViewModel
+            {
+                get => ViewModel;
+                set => ViewModel = (TestVm?)value;
+            }
+        }
+
+        [ViewContract("C1")]
+        private sealed class ContractedTestView : UserControl, IViewFor<TestVm>
+        {
+            public TestVm? ViewModel { get; set; }
+
+            object? IViewFor.ViewModel
+            {
+                get => ViewModel;
+                set => ViewModel = (TestVm?)value;
+            }
+        }
+
+        private sealed class DiBackedView : UserControl, IViewFor<TestVm>
+        {
+            public TestVm? ViewModel { get; set; }
+
+            object? IViewFor.ViewModel
+            {
+                get => ViewModel;
+                set => ViewModel = (TestVm?)value;
+            }
+        }
+    }
+}
---
+++ src/Splat.Avalonia.Tests/AvaloniaCreatesCommandBindingTests.cs
@@ -0,0 +1,115 @@
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using NUnit.Framework;
+
+namespace ReactiveUI.Avalonia.Tests
+{
+    public class AvaloniaCreatesCommandBindingTests
+    {
+        [Test]
+        public void GetAffinityForObject_Returns_Expected()
+        {
+            var sut = new AvaloniaCreatesCommandBinding();
+
+            Assert.That(sut.GetAffinityForObject(typeof(object), hasEventTarget: false), Is.EqualTo(0));
+            Assert.That(sut.GetAffinityForObject(typeof(InputElement), hasEventTarget: false), Is.EqualTo(0));
+            Assert.That(sut.GetAffinityForObject(typeof(InputElement), hasEventTarget: true), Is.GreaterThan(0));
+            Assert.That(sut.GetAffinityForObject(typeof(Button), hasEventTarget: false), Is.EqualTo(10));
+        }
+
+        [Test]
+        public void BindCommandToObject_Wires_Button_Command_And_Parameter()
+        {
+            var sut = new AvaloniaCreatesCommandBinding();
+            var cmd = new TestCommand();
+            var btn = new Button();
+            var param = new BehaviorSubject<object?>("p1");
+
+            using var disp = sut.BindCommandToObject(cmd, btn, param)!;
+
+            Assert.That(btn.CommandParameter, Is.EqualTo("p1"));
+
+            param.OnNext("p2");
+            Assert.That(btn.CommandParameter, Is.EqualTo("p2"));
+
+            // Sanity check that binding exists
+            Assert.That(btn.Command, Is.Not.Null);
+
+            disp.Dispose();
+            Assert.That(btn.Command, Is.Null);
+        }
+
+        [Test]
+        public void BindCommandToObject_With_EventName_Binds_InputElement_GotFocus()
+        {
+            var sut = new AvaloniaCreatesCommandBinding();
+            var cmd = new TestCommand();
+            var btn = new Button();
+            var param = new BehaviorSubject<object?>("evt");
+
+            using var disp = sut.BindCommandToObject<RoutedEventArgs>(cmd, btn, param, nameof(InputElement.GotFocus));
+
+            Assert.That(btn.IsEnabled, Is.True);
+
+            cmd.SetCanExecute(false);
+            param.OnNext("evt2");
+            Assert.That(btn.IsEnabled, Is.False);
+
+            cmd.SetCanExecute(true);
+            param.OnNext("evt3");
+            Assert.That(btn.IsEnabled, Is.True);
+
+            btn.RaiseEvent(new RoutedEventArgs(InputElement.GotFocusEvent));
+            Assert.That(cmd.ExecutedCount, Is.EqualTo(1));
+            Assert.That(cmd.LastParameter, Is.EqualTo("evt3"));
+
+            disp.Dispose();
+            Assert.That(btn.IsSet(InputElement.IsEnabledProperty), Is.False);
+        }
+
+        [Test]
+        public void BindCommandToObject_Throws_On_Invalid_Targets_Or_Nulls()
+        {
+            var sut = new AvaloniaCreatesCommandBinding();
+            var cmd = new TestCommand();
+            var param = new BehaviorSubject<object?>(null);
+
+            Assert.Throws<ArgumentNullException>(() => sut.BindCommandToObject(null!, new object(), param));
+            Assert.Throws<ArgumentNullException>(() => sut.BindCommandToObject(cmd, null!, param));
+            Assert.Throws<InvalidOperationException>(() => sut.BindCommandToObject(cmd, new object(), param));
+
+            Assert.Throws<ArgumentNullException>(() => sut.BindCommandToObject<RoutedEventArgs>(null!, new object(), param, "Click"));
+            Assert.Throws<ArgumentNullException>(() => sut.BindCommandToObject<RoutedEventArgs>(cmd, null!, param, "Click"));
+            Assert.Throws<InvalidOperationException>(() => sut.BindCommandToObject<RoutedEventArgs>(cmd, new object(), param, "Click"));
+            Assert.Throws<InvalidOperationException>(() => sut.BindCommandToObject<RoutedEventArgs>(cmd, new Button(), param, "MissingEvent"));
+        }
+
+        private sealed class TestCommand : System.Windows.Input.ICommand
+        {
+            private bool _canExecute = true;
+
+            public event EventHandler? CanExecuteChanged;
+
+            public int ExecutedCount { get; private set; }
+
+            public object? LastParameter { get; private set; }
+
+            public void SetCanExecute(bool can)
+            {
+                _canExecute = can;
+                CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+            }
+
+            public bool CanExecute(object? parameter) => _canExecute;
+
+            public void Execute(object? parameter)
+            {
+                ExecutedCount++;
+                LastParameter = parameter;
+            }
+        }
+    }
+}
